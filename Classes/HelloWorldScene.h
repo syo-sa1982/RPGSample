0ad703ba79e751e25a8dbe123eefabbd62152540
藤井 陽介 <fujiiyousuke@SyousaMacBookAir.local>
2013-10-08 21:38:34 +0900

    マップ修正

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -3,6 +3,8 @@
 
 #include "cocos2d.h"
 
+USING_NS_CC;
+
 class HelloWorld : public cocos2d::CCLayer
 {
 public:
@@ -12,11 +14,12 @@ public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::CCScene* scene();
     
-    // a selector callback
-    void menuCloseCallback(CCObject* pSender);
-    
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
+    
+private:
+    CCTMXTiledMap *_tileMap;
+    
 };
 
 #endif // __HELLOWORLD_SCENE_H__
